a766703131c7ee7d4fa8eaf53c371505711f2290
rafahahon <rafaellapintohahon@gmail.com>
2025-09-03 10:00:34 -0300

    Introdução ao JS, alguns códigos iniciais

---
+++ exercicios.js
@@ -0,0 +1,34 @@
+// Nível básico
+
+// Exercício 1: Verificar maioridade:
+
+
+// Exercicio 2: Verificar se um número é positivo ou negativo:
+
+// Exercicio 3: Aprovação em uma prova:
+
+// Exercicio 4: Verificar se um número é positivo, negativo ou zero:
+
+// Exercicio 5: Classificação de idade:
+
+// Exercicio 6: Verificar se um número é par ou ímpar:
+
+// Nível intermediário
+
+// Exercicio 1: Calculadora simples:
+
+// Exercicio 2: Maior entre três números:
+
+// Exercicio 3: Desconto em compras:
+
+// Exercicio 4: Sistema de login simples:
+
+// Nível avançado
+
+// Exercicio 1: Classificação de triângulos:
+
+// Exercicio 2: Conversão de notas para conceitos:
+
+// Exercicio 3: Cálculo de IMC:
+
+// Exercicio 4: Validação de ano bissexto:
